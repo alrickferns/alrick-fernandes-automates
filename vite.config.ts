083739351b356d676f4d186b313c4863f6facc6f
alrickferns <143488387+alrickferns@users.noreply.github.com>
2025-06-30 20:12:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
+    mode === 'production' &&
     componentTagger(),
   ].filter(Boolean),
   resolve: {
